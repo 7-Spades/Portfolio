83e9ac52bcda656897f6cb4f3486b0c915ebf6eb
7-Spades <edwin5106@gmail.com>
2024-03-16 01:41:57 -0500

    Add files via upload

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./public/*/**.html",
+    "./public/index.html"
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
